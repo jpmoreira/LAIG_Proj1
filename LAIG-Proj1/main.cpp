6b3f4aa86f8db8e0d366dacbb68a565ff3a58b17
Jose Moreira <ei12002@fe.up.pt>
2014-09-22 11:39:09 +0100

    Initial Commit

---
+++ LAIG-Proj1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LAIG-Proj1
+//
+//  Created by José Pedro Moreira on 22/09/14.
+//  Copyright (c) 2014 José Pedro Moreira. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
