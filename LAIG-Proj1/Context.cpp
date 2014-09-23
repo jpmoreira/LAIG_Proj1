8c804acfeb22634bf046ee864c6e66b3429667de
Jose Moreira <ei12002@fe.up.pt>
2014-09-23 08:22:07 +0100

    Initial Commit

--- LAIG-Proj1/Context.cpp
+++ LAIG-Proj1/Context.cpp
@@ -7,3 +7,113 @@
 //
 
 #include "Context.h"
+
+
+
+
+
+#pragma mark - Constructors
+
+
+LG_Context::LG_Context():texture(NULL){
+    
+    loadIdentity();
+
+
+    
+    
+
+}
+
+
+#pragma mark - Cloning
+
+LG_Context * LG_Context::clone(){
+
+    LG_Context * newContext=new LG_Context();
+    
+    newContext->cloneMatrix(this);
+    
+    
+    
+    
+    return newContext;
+    
+
+}
+
+
+/**
+ 
+ Clones a Context object but changes it's light component.
+ 
+ */
+LG_Context clone_changeLight(GLdouble amb[4],GLdouble diff[4],GLdouble spec[4],GLdouble shininess);
+
+
+/**
+ 
+ Clones a Context object but changes the texture.
+ 
+ 
+ */
+LG_Context clone_changeTexture(CGFtexture *newTexture);
+
+
+/**
+ 
+ 
+ Clones a context but changes the matrix for this context.
+ 
+ */
+LG_Context clone_changeMatrix(GLdouble newMatrix[4][4]);
+
+/**
+ 
+ Clones a context but applies a rotation to the current matrix.
+ 
+ */
+LG_Context clone_Rotate(double angle,LG_Axis axis);
+
+
+/**
+ 
+ Clones a context but applies a translation to the current matrix.
+ 
+ */
+LG_Context clone_Translate(double dx,double dy, double dz);
+
+/**
+ 
+ Clones a context but applies a scaling operation to the current matrix.
+ 
+ */
+LG_Context clone_Scale(double sx,double sy,double sz);
+
+
+#pragma mark - Helper Methods
+
+
+void LG_Context::loadIdentity(){
+
+    for (int col=0; col<4; col++) {
+        for (int line=0; line<4; line++) {
+            
+            if (line==col) matrix[col][line]=1;
+            else matrix[col][line]=0;
+            
+        }
+    }
+
+}
+
+
+void LG_Context::cloneMatrix(LG_Context * contextToClone){
+    
+    for (int col=0; col<4; col++) {
+        for (int line=0; line<4; line++) {
+            matrix[col][line]=contextToClone->matrix[col][line];
+        }
+    }
+
+}
--- LAIG-Proj1/Context.h
+++ LAIG-Proj1/Context.h
@@ -23,7 +23,17 @@ typedef enum{
 
 } LG_Axis;
 
-class Context
+
+typedef GLdouble LG_LightArray[4];
+
+
+/**
+ 
+ An Object to hold all the information related to the current state when iterating through the graph structure.
+ 
+ */
+
+class LG_Context
 {
 
     GLdouble  matrix[4][4];
@@ -36,16 +46,85 @@ class Context
     
 public:
     
-    Context(GLdouble *matrix,GLdouble amb[4],GLdouble diff[4],GLdouble spec[4],GLdouble shininess);
+    
+    LG_Context();
+    
+    LG_Context(GLdouble *matrix,GLdouble amb[4],GLdouble diff[4],GLdouble spec[4],GLdouble shininess);
     void setTexture(CGFtexture *newText);
     
-    Context clone();
-    Context clone_changeLight(GLdouble amb[4],GLdouble diff[4],GLdouble spec[4],GLdouble shininess);
-    Context clone_changeTexture(CGFtexture *newTexture);
-    Context clone_changeMatrix(GLdouble newMatrix[4][4]);
-    Context clone_Rotate(double angle,LG_Axis axis);
-    Context clone_Translate(double dx,double dy, double dz);
-    Context clone_Scale(double sx,double sy,double sz);
+    
+    
+    /**
+     * Clones a context. Creates a copy of the context.
+     *
+    */
+    LG_Context * clone();
+    
+    
+    /**
+     
+     Clones a Context object but changes it's light component.
+     
+     */
+    LG_Context * clone_changeLight(GLdouble amb[4],GLdouble diff[4],GLdouble spec[4],GLdouble shininess);
+    
+    
+    /**
+     
+     Clones a Context object but changes the texture.
+     
+     
+     */
+    LG_Context * clone_changeTexture(CGFtexture *newTexture);
+    
+    
+    /**
+     
+     
+     Clones a context but changes the matrix for this context.
+     
+     */
+    LG_Context * clone_changeMatrix(GLdouble newMatrix[4][4]);
+    
+    /**
+     
+     Clones a context but applies a rotation to the current matrix.
+     
+     */
+    LG_Context * clone_Rotate(double angle,LG_Axis axis);
+    
+    
+    /**
+     
+     Clones a context but applies a translation to the current matrix.
+     
+     */
+    LG_Context * clone_Translate(double dx,double dy, double dz);
+    
+    /**
+     
+     Clones a context but applies a scaling operation to the current matrix.
+     
+     */
+    LG_Context * clone_Scale(double sx,double sy,double sz);
     
 
+    
+private:
+    /**
+     
+     Loads the identity matrix onto the matrix data member of this object
+     
+     
+     */
+    void loadIdentity();
+    
+    /**
+     
+     Clones the other contexts matrix
+     
+     */
+    void cloneMatrix(LG_Context *contextToClone);
+    
+    void cloneLightArray(GLdouble * lightArray);
 };
