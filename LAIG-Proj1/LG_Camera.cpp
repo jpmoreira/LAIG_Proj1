d500f81257e335622b542d09b59d2d6e269e5f8d
Jose Moreira <ei12002@fe.up.pt>
2014-10-13 19:38:04 +0100

    Implemented Prespective camera

--- LAIG-Proj1/LG_Camera.cpp
+++ LAIG-Proj1/LG_Camera.cpp
@@ -1,10 +1,13 @@
 #include "LG_Camera.h"
 #include "LG_Cameras_Container.h"
 
+#include <GL/glut.h>
+
 LG_Camera::LG_Camera(LG_Node_Map *map, TiXmlElement *element, string identifier) : LG_Parsable_Node(map, identifier)
 {
 	double_tryToAttributeVariable(LG_Camera_XML_ATT_NEAR, element, _near);
 	double_tryToAttributeVariable(LG_Camera_XML_ATT_FAR, element, _far);
+    
 
 }
 
@@ -31,4 +34,24 @@ string LG_Camera::identifierForSuper(TiXmlElement *element){
 	string_tryToAttributeVariable(LG_Camera_XML_ATT_ID, element, identifier);
 
 	return identifier;
+}
+
+void LG_Camera::applyView(){
+    
+    
+    gluLookAt(0, 0, 0, 0, 0, -1, 0, 1, 0);
+    
+    
+    
+    // glTranslatef(0, 0, position[2]); // or this, if we ignore panning
+    
+    glRotatef(rotation[0], 1.f, 0.f, 0.f);
+    glRotatef(rotation[1], 0.f, 1.f, 0.f);
+    glRotatef(rotation[2], 0.f, 0.f, 1.f);
+    
+    
+    glTranslatef(position[0],position[1], position[2]);
+    //gluLookAt(0, 0, 0, 0, 0, 1, 0, 1, 0);
+    //CGFcamera::applyView();
+    
 }
